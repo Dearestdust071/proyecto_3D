706ab6f0dad2a4c706e54332cd4f7a5624ce31c2
dearestdust071@gmail.com <dearestdust071@gmail.com>
2023-10-23 19:27:13 -0600

    de cero

--- counter.js
+++ counter.js
@@ -1,9 +1,96 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+import './style.css'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// Tres cosas son necesarias para una escena en 3D 
+
+
+
+
+
+// * 1) La escena 2) La camara 3) Lo que se renderizara el render 
+const escena = new THREE.Scene();
+const camara = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camara.position.set(0, 3, 6)
+const render =new THREE.WebGLRenderer(); 
+render.setSize(window.innerWidth, window.innerHeight);
+render.shadowMap.enabled = true
+document.body.appendChild(render.domElement)
+escena.background = new THREE.Color("rgb(200,16,16)");
+document.body.appendChild( render.domElement );
+
+
+// const light = new THREE.AmbientLight( 0xFFFF ); // soft white light
+// escena.add( light );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
+// directionalLight.position(0,0,0)
+escena.add( directionalLight );
+
+
+const controls = new OrbitControls(camara, render.domElement)
+
+const loader = new GLTFLoader();
+
+let tails;
+loader.load( 'models/tails/tails.gltf', function ( gltf ) {
+    tails = gltf.scene;
+    tails.position.x = -4;
+    tails.scale.set(1, 1, 1);
+    tails.position.set(0,1,0)
+    tails.castShadow = true;
+	escena.add( tails );
+    
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+
+let olivia;
+loader.load( 'models/olivia/olivia.gltf', function ( gltf ) {
+    olivia = gltf.scene;
+    // olivia.position.x = -4;
+    olivia.scale.set(1, 1, 1);
+	escena.add( olivia );
+    
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+
+let demonio;
+loader.load( 'models/demonio/demonio.gltf', function ( gltf ) {
+    demonio = gltf.scene;
+    demonio.position.z = -4;
+    demonio.scale.set(1, 1, 1);
+	escena.add( demonio );
+    
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+
+
+
+function animar(){
+    requestAnimationFrame( animar)
+    render.render( escena,camara );
+
+
 }
+
+
+const geometry = new THREE.PlaneGeometry( 1, 1 );
+const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const plane = new THREE.Mesh( geometry, material );
+escena.add( plane );
+
+animar();
+
+
+
--- main.js
+++ main.js
@@ -1,62 +1,37 @@
-import './style.css'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-// Tres cosas son necesarias para una escena en 3D 
-
-
-// * 1) La escena 2) La camara 3) Lo que se renderizara el render 
-const escena = new THREE.Scene();
-const camara = new THREE.PerspectiveCamera(80, window.innerWidth/window.innerHeight, .1, 500);
-const render =new THREE.WebGLRenderer();
-render.setSize(window.innerWidth, window.innerHeight);
-escena.background = new THREE.Color("rgb(200,16,16)");
-document.body.appendChild( render.domElement );
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
-const ambientLigth = new THREE.AmbientLight(0xffffff);
-escena.add(ambientLigth);
+const scene = new THREE.Scene();
+const camara = new THREE.PerspectiveCamera(80, window.innerHeight/window.innerWidth, 1, 100);
+const render = new THREE.WebGLRenderer();
+const controls = new OrbitControls( camara, render.domElement );
+render.setSize(window.innerHeight/window.innerWidth);
 
-const geometria = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: "blue"});
-const caja = new THREE.Mesh(geometria,material);
-const caja2 = new THREE.Mesh(geometria,material);
-const caja3 = new THREE.Mesh(geometria,material);
 
-const loader = new GLTFLoader();
-let perro;
-loader.load( 'models/dona/Dona.gltf', function ( gltf ) {
-    perro = gltf.scene;
-    perro.scale.set(30, 30, 30);
-	escena.add( perro );
-    
-}, undefined, function ( error ) {
+scene.add(camara);
+scene.add(render);
 
-	console.error( error );
+document.appendChild(render);
 
-} );
 
-escena.add(caja)
-escena.add(caja2)
-escena.add(caja3)
+const geometria = new THREE.PlaneGeometry(20,10);
+const material = new THREE.MeshBasicMaterial('red')
+const plano = new THREE.Mesh(geometria, material);
+scene.add(plano);
 
-caja2.position.x = 2;
-caja3.position.x = -2;
-camara.position.z = 5;
+function animate(){
+    requestAnimationFrame( animate )
+    animate()
+
+    controls.update();
 
 
 
-function animar(){
-    requestAnimationFrame( animar)
-    render.render( escena,camara );
-    caja.rotation.x += .01
-    caja.rotation.z += .01
-    caja2.rotateX(.01)
-    caja3.rotateY(.01)
-    perro.rotateY(.01)
+    render.render(scene,camara)
+
+
 }
 
-animar();
-
-
-
+animate()
